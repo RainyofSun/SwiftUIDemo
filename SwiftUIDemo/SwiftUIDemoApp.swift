7bab627beb1ddd6fa8fa9185c617e94abe306b67
RainyofSun <RainyofSun>
2023-04-07 14:35:05 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by 苍蓝猛兽 on 2023/4/7.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
